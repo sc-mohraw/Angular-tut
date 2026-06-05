0b0443a05c1cfa5005b5a8460d5308114c941b2e
Mohit <mohit.rawat@scryai.com>
2026-06-05 13:12:04 +0530

    pipes

--- src/app/components/user/user.ts
+++ src/app/components/user/user.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
 import { WarningComponent } from "../reusablecomponents/warning-component/warning-component";
+import { DatePipe, LowerCasePipe, SlicePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-user',
-  imports: [WarningComponent],
+  imports: [WarningComponent, UpperCasePipe, LowerCasePipe, TitleCasePipe, SlicePipe, DatePipe],
   templateUrl: './user.html',
   styleUrl: './user.css',
 })
 export class User {
   name = "Mohit"
-  age = "24"
+  age = 24
+  marks = [90, 87, 85, 92, 78, 84, 64, 54, 87]
+  currentDate: Date = new Date();
+
   receiveMessage(data: string) {
     console.log(data)
   }
